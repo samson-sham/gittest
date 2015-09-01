a94226a4fc0ea2985866a5cf18894dc7429fc142
S <S@Ss-MacBook-Air.local>
2015-09-01 23:27:07 +0800

    First commit

---
+++ test.js
@@ -0,0 +1,19 @@
+// var childProcess = require("child_process");
+// var gitProcess = childProcess.spawn("prompt.js");
+// var gitProcess = childProcess.fork("prompt.js");
+
+// gitProcess.stdin.setEncoding('utf8');
+// gitProcess.stdout.pipe(process.stdout);
+
+
+// 	gitProcess = childProcess.fork("prompt.js");
+
+// gitProcess.stdout.on('data', function(data) {
+// 	console.log('stdout: '+data);
+// });
+
+// childProcess.exec("node prompt.js", function() {
+
+// });
+
+console.log(require.resolve("promise"));
