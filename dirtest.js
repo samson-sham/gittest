de5cb26235fd5e6072224dcf632906f9fd665c33
samsonsham <samson.sham@accedo.tv>
2015-12-10 15:04:33 +0800

    Added: dirtest.js

---
+++ dirtest.js
@@ -0,0 +1,40 @@
+// var fs = require("fs"),
+var fs = require("graceful-fs"),
+	FileType = require("file-type"),
+	// ReadChunk = require("read-chunk"),
+	target = "/Users/samson/Downloads";
+
+fs.readdir(target, function (error, directoryList) {
+	console.log("Target: "+target, directoryList.length);
+	var i = 1;
+	directoryList.forEach(function (directory, index) {
+		// ReadChunk(target+'/'+directory, 0, 262, function (error, buffer) {
+		// 	var fileType = FileType(buffer);
+		// 	console.log("fileType: "+(fileType && fileType.mime)+" file: "+directory);
+		// });
+		var fullpath = target+'/'+directory;
+		fs.stat(fullpath, (function (index, fullpath, error, stats) {
+			if (error) return console.log("File stat error:", error);
+			if (!stats.isFile()) return;
+			// console.log(i+">"+directory);
+			// i++;
+			fs.open(fullpath, 'r', (function (index, error, fileDescriptor) {
+				if (error) return console.log("File open error:", error);
+				var whatForBuffer = new Buffer(262);
+				fs.read(fileDescriptor, whatForBuffer, 0, 262, void 0, (function (index, error, bytesRead, buffer) {
+					if (error) return console.log("File read error:", error, directory);
+					var fileType = FileType(buffer);
+					console.log(index+">fileType: "+(fileType && fileType.mime)+" file: "+directory+ " reads: "+bytesRead);
+					fs.close(fileDescriptor, function () {});
+				}).bind(void 0, index));
+			}).bind(void 0, index));
+		}).bind(void 0, index, fullpath));
+	});
+});
+
+// var directoryList = fs.readdirSync(target);
+// directoryList.forEach(function (directory) {
+// 	var buffer = ReadChunk.sync(target+'/'+directory, 0, 262);
+// 	var fileType = FileType(buffer);
+// 	console.log("fileType: "+(fileType && fileType.mime)+" file: "+directory);
+// });
