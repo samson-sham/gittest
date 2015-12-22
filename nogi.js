a1e5c38b6330cd5186b98bb80c42c02678573dd3
samsonsham <samson.sham@accedo.tv>
2015-12-22 15:12:31 +0800

    Add: nogi fetch img

--- nogi.js
+++ nogi.js
@@ -40,36 +40,73 @@ var gm = require("gm"),
 	stream = fs.createReadStream("test.html"),
 	body;
 
-// stream.on('data', function (line) {
-// 	body += line;
-// }).on('end', function() {
-// 	var $ = cheerio.load(body),
-// 		images = $(".entrybody").find("img");
+stream.on('data', function (line) {
+	body += line;
+}).on('end', function() {
+	var $ = cheerio.load(body),
+		images = $(".entrybody").find("img");
 
-// 	images.each(function (index, element) {
-// 		// console.log($(this).attr("src"));
-// 		// var src = url.parse($(this).attr("src")),
+	images.each(function (index, element) {
+		// var src = url.parse($(this).attr("src")),
 
-// 		// http.request({
-// 		// 	host: src.host,
-// 		// 	path: src.path,
-// 		// 	port: src.port,
-// 		// 	method: 'GET'
-// 		// }
-// 		http.request($(this).attr("src"), (function (src, response) {
-// 			console.log(response.statusCode, src);
-// 			gm(response).size(function (error, size) {
-// 				if (error) return console.log("Error:", error);
-// 				console.log(src, size.width, size.height);
-// 				// if (size.width < 150 || size.height < 150);
-// 			});
-// 		}).bind(void 0, $(this).attr("src"))).on('error', function (error) {
-// 			console.log("Error:", error);
-// 		}).end();
-// 	});
+		// http.request({
+		// 	host: src.host,
+		// 	path: src.path,
+		// 	port: src.port,
+		// 	method: 'GET'
+		// }
+
+		// http.request($(this).attr("src"), (function (src, response) {
+		// 	console.log(response.statusCode, src);
+		// 	// Check if it is a photo
+		// 	// gm(response).size(function (error, size) {
+		// 	// 	if (error) return console.log("Error:", error);
+		// 	// 	console.log(src, size.width, size.height);
+		// 	// 	// if (size.width < 150 || size.height < 150);
+		// 	// });
+		// }).bind(void 0, $(this).attr("src"))).on('error', function (error) {
+		// 	console.log("Error:", error);
+		// }).end();
+
+		// Download all linked image
+		if ($(this).parent().is("a")) {
+			// console.log($(this).parent().attr("href"));
+			var src = $(this).parent().attr("href");
+			http.request(src, (function (response) {
+				console.log(response.statusCode, src);
+				console.log(response.headers);
+				var cookie = response.headers['set-cookie'][0],
+					filename = url.parse($(this).attr("src")).pathname.replace(/blog|\/|\.(?=.*\.)/g,'');
+				src = url.parse(src.replace('img1', 'img2').replace('id=', 'sec_key='));
+				console.log(src);
+				// http.request({
+				// 	host: src.host,
+				// 	path: src.path,
+				// 	port: src.port,
+				// 	headers: {
+				// 		'Cookie': cookie
+				// 	},
+				// 	method: 'GET'
+				// }, function (response) {
+				// 	console.log(response.statusCode, src);
+				// 	if (response.statusCode !== 200) return;
+				// 	var writeStream = fs.createWriteStream('test.jpg');
+				// 	response.pipe(writeStream);
+				// 	response.on('end', function() {
+				// 		console.log("Save successful!");
+				// 	});
+				// }).on('error', function (error) {
+				// 	console.log("Error:", error);
+				// }).end();
+			}).bind(this)).on('error', function (error) {
+				console.log("Error:", error);
+			}).end();
+			return false;	// @DEBUG
+		}
+	});
+});
+
+// gm("/Users/samson/Desktop/Screen Shot 2015-11-26 at 1.51.11 pm.png").size(function (error, size) {
+// 	if (error) return console.log("Error:", error);
+// 	console.log(size);
 // });
-
-gm("/Users/samson/Desktop/Screen Shot 2015-11-26 at 1.51.11 pm.png").size(function (error, size) {
-	if (error) return console.log("Error:", error);
-	console.log(size);
-})
